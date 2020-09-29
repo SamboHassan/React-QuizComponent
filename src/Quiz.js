15b750a88df0c294b108fad77059da2d2d95093b
EngrAdam <engr.adam77@gmail.com>
2020-09-29 17:23:03 +0100

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestion.js'
 
 
 
@@ -14,12 +15,9 @@ class Quiz extends Component {
   }
 
   render() {
-    const instruction_text = this.instruction_text;
     return(
       <div>
-        <div className="QuizQuestion">
-          {quizData.quiz_questions[0].instruction_text}
-        </div>
+        <QuizQuestion quiz_question="quizData.quiz_questions[quiz_position-1]"/>
       </div>
     )
   }
